2d50a2e1ca0281638907597fab61ce2ac5ad1dd0
ethmth <ethan@smaze.net>
2023-06-15 13:51:39 -0400

    Updated phidget installers

--- scripts/scooter/05-installers/01-phidget.sh
+++ scripts/scooter/05-installers/01-phidget.sh
@@ -15,6 +15,7 @@ fi
 source $SCRIPT_DIR/../conf.env
 
 DIR="$INSTALLATION_DIR/build/phidget"
+rm -rf $DIR
 mkdir -p $DIR
 
 wget -O $DIR/libphidget.tar.gz https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22.tar.gz
@@ -22,7 +23,7 @@ cd $DIR
 tar -xvf libphidget.tar.gz
 DIR_NAME=$(ls $DIR -1 | grep -v "tar.gz")
 cd $DIR/$DIR_NAME
-bash $DIR/$DIR_NAME/configure
+./configure
 make
 sudo make install
 
@@ -31,7 +32,7 @@ cd $DIR
 tar -xvf libphidgetextra.tar.gz
 DIR_NAME=$(ls $DIR -1 | grep "extra" | grep -v "tar.gz")
 cd $DIR/$DIR_NAME
-bash $DIR/$DIR_NAME/configure
+./configure
 make
 sudo make install
 
@@ -40,7 +41,7 @@ cd $DIR
 tar -xvf phidgetnetworkserver.tar.gz
 DIR_NAME=$(ls $DIR -1 | grep "networkserver" | grep -v "tar.gz")
 cd $DIR/$DIR_NAME
-bash $DIR/$DIR_NAME/configure
+./configure
 make
 sudo make install
 
@@ -49,10 +50,16 @@ cd $DIR
 tar -xvf phidgetadmin.tar.gz
 DIR_NAME=$(ls $DIR -1 | grep "admin" | grep -v "tar.gz")
 cd $DIR/$DIR_NAME
-bash $DIR/$DIR_NAME/configure
+./configure
 make
 sudo make install
 
-$PYTHON_COMMAND -m pip install Phidget22
-
 sudo bash -c 'echo "SUBSYSTEMS==\"usb\", ACTION==\"add\", ATTRS{idVendor}==\"06c2\", ATTRS{idProduct}==\"00[3-a][0-f]\", MODE=\"666\"" > /etc/udev/rules.d/99-libphidget22.rules'
+
+#$PYTHON_COMMAND -m pip install Phidget22
+wget -O $DIR/Phidget22Python.zip https://www.phidgets.com/downloads/phidget22/libraries/any/Phidget22Python.zip
+cd $DIR
+unzip Phidget22Python.zip
+DIR_NAME=$(ls $DIR -1 | grep "Python" | grep -v "zip")
+cd $DIR/$DIR_NAME
+sudo $PYTHON_COMMAND setup.py install
---
+++ scripts/scooter/06-repos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GATT_REPO="https://github.com/ykasidit/bluez-gatt-server.git"
+SCOOTER_REPO="https://github.com/ethmth/e-scooter.git"
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should not be run with root/sudo privileges."
+        exit 1
+fi
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+if ! [ -e "$DIR/conf.env" ]; then
+	echo "Be sure to generate the configuration file $DIR/conf.env using ./03-config.sh"
+	exit 1
+fi
+
+source $DIR/conf.env
+
+mkdir -p $INSTALLATION_DIR
+
+cd $INSTALLATION_DIR
+git clone $GATT_REPO bluez-gatt-server
+git clone $SCOOTER_REPO e-scooter
+
+cd $INSTALLATION_DIR/e-scooter
+./gen-types.sh
+
+
