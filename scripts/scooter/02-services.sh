46a919d2000e7114ba9f119b602aad12d34cbe60
ethmth <ethan@smaze.net>
2023-06-15 20:07:58 -0400

    Removed unneeded services script

--- scripts/scooter/02-services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $EUID -ne 0 ]]; then
-        echo "This script should be run with root/sudo privileges."
-        exit 1
-fi
-
-systemctl start mosquitto
-systemctl enable mosquitto
