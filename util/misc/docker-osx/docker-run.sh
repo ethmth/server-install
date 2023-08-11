0112d93d8c808491d775d5f9cf49f9925aa26f48
ethmth <ethan@smaze.net>
2023-08-11 11:19:35 -0400

    Attempted vnc in docker run script

--- util/misc/docker-osx/docker-run.sh
+++ util/misc/docker-osx/docker-run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker run \
+docker run -i \
+    --device /dev/kvm \
+    -p 5900:99 \
     -e RAM=4 \
     -e SMP=4 \
     -e CORES=2 \
-    -e MAC_ADDRESS="00:25:4B:59:36:0D" \
-    -e SCREEN_SHARE_PORT=5900 \
-    --device /dev/kvm \
-    --device /dev/snd \
+    -e AUDIO_DRIVER=none \
+    -e MAC_ADDRESS="00:11:22:33:44:55" \
+    -e EXTRA="-display none -vnc 0.0.0.0:99,password=on" \
     sickcodes/docker-osx:high-sierra
