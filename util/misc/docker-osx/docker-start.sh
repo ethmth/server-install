65a8edc2b3528cf3ea51c50a0b4025f9bb170cf9
ethmth <ethan@smaze.net>
2023-08-12 15:21:34 -0400

    Stuck with no boot entries

--- util/misc/docker-osx/docker-start.sh
+++ util/misc/docker-osx/docker-start.sh
@@ -9,13 +9,14 @@ docker run \
   -p 1359:1359 \
   -p 50922:10022 \
   -v ./mac_hdd_ng.img:/image \
-  -v ./OpenCore.qcow2:/bootdisk \
+#   -v ./OpenCore.qcow2:/bootdisk \
+  -e AUDIO_DRIVER=none \
   -e IMAGE_PATH="/image" \
-  -e BOOTDISK="/bootdisk" \
+#   -e BOOTDISK="/bootdisk" \
   -e EXTRA="-display none -vnc 0.0.0.0:99,password-secret=secvnc0 -object secret,id=secvnc0,data=vncpass" \
   -e ADDITIONAL_PORTS="hostfwd=tcp::1234-:1234,hostfwd=tcp::1359-:1359," \
-  -e DISPLAY=:99 \
+#   -e DISPLAY=:99 \
   -e WIDTH=1280 \
   -e HEIGHT=720 \
-  -e NOPICKER=true \
+#   -e NOPICKER=true \
   sickcodes/docker-osx:naked
