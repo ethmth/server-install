1bc23165c11029f81bc062ff64f0ae2688120839
ethmth <ethan@smaze.net>
2023-08-12 15:14:33 -0400

    Added image conversion script

--- util/misc/docker-osx/docker-start.sh
+++ util/misc/docker-osx/docker-start.sh
@@ -3,20 +3,19 @@
 docker run \
   --rm \
   --name bluebubbles \
-  --dns=1.1.1.1 \
   --device /dev/kvm \
   -p 5999:5999 \
   -p 1234:1234 \
+  -p 1359:1359 \
   -p 50922:10022 \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v $PWD/maindisk.qcow2:/image \
-  -v $PWD/bootdisk.qcow2:/bootdisk \
+  -v ./mac_hdd_ng.img:/image \
+  -v ./OpenCore.qcow2:/bootdisk \
   -e IMAGE_PATH="/image" \
   -e BOOTDISK="/bootdisk" \
   -e EXTRA="-display none -vnc 0.0.0.0:99,password-secret=secvnc0 -object secret,id=secvnc0,data=vncpass" \
-  -e ADDITIONAL_PORTS="hostfwd=tcp::1234-:1234," \
+  -e ADDITIONAL_PORTS="hostfwd=tcp::1234-:1234,hostfwd=tcp::1359-:1359," \
   -e DISPLAY=:99 \
-  -e WIDTH=1920 \
-  -e HEIGHT=1080 \
+  -e WIDTH=1280 \
+  -e HEIGHT=720 \
   -e NOPICKER=true \
   sickcodes/docker-osx:naked
