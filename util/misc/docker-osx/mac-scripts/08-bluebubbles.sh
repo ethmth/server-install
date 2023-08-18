5137202a6d11722f18cd6d90180b340ee6db5232
ethmth <ethan@smaze.net>
2023-08-18 17:06:40 -0400

    Added mac screen saver note

--- util/misc/docker-osx/mac-scripts/08-bluebubbles.sh
+++ util/misc/docker-osx/mac-scripts/08-bluebubbles.sh
@@ -12,6 +12,7 @@ echo "=============== PERMISSIONS ================="
 echo "1. Ensure BlueBubbles has Accessibility permission in System Preferences > Security & Privacy > Privacy > Accessibility"
 echo "2. Under System Preferences > Security & Privacy > Privacy > Full Disk Access, add BlueBubbles"
 echo "3. System Preferences > Energy Saver. Change energy settings to prevent shutdown"
+echo "4. System Preferences > Desktop & Screen Saver > Screen Saver > Start after: Never"
 
 echo "=============== STARTUP ================="
 echo "1. System Preferences > Users & Groups > user > Login Items. Include iMessage, BlueBubbles, and AirMessage."
