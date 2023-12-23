d4837f36ecf7a4d9edcd8e98f8134b8ae58e71a9
ethmth <ethan@smaze.net>
2023-12-23 14:07:51 -0500

    Setup droplet scripts

---
+++ scripts/droplet/01-setup.sh
@@ -0,0 +1,27 @@
+
+
+# Login as root
+apt update && apt upgrade -y
+
+# Edit /etc/sudoers to include:
+echo "%sudo	ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers.d/70-sudo-group
+
+# Create a user
+useradd -m e
+usermod -aG sudo e
+chsh -s /bin/bash e
+passwd e
+# Then, re-login to user account
+
+# Setup ssh key authentication
+
+# Harden SSH
+sudo sh -c "echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/10-disable-root.conf"
+sudo systemctl restart sshd
+
+
+# Setup IPtables rules to block non-SSH, non-HTTP traffic
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables -A INPUT -j DROP
