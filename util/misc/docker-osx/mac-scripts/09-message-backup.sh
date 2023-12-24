fa4ff114e9ed3293e85d09f1303a187ef5e07abc
ethmth <ethan@smaze.net>
2023-12-23 22:54:41 -0500

    Fixed message extraction scripts

--- util/misc/docker-osx/mac-scripts/09-message-backup.sh
+++ util/misc/docker-osx/mac-scripts/09-message-backup.sh
@@ -3,7 +3,8 @@
 git clone --depth 1 https://github.com/cfinke/OSX-Messages-Exporter.git
 cp ./OSX-Messages-Exporter/messages-exporter.php ${HOME}/bin/messages-exporter.php
 
-echo "php $HOME/bin/messages-exporter.php -o ~/Desktop/Messages/" ${HOME}/bin/message-backup
+echo "#!/bin/sh
+php $HOME/bin/messages-exporter.php -o ~/Desktop/Messages/" > ${HOME}/bin/message-backup
 chmod +rx ${HOME}/bin/message-backup
 
 echo "Run 'message-backup' to backup iMessages to Desktop"
