c8f6cf874619a97c5a2ff06c5705b19436767b84
ethmth <ethan@smaze.net>
2023-12-23 21:12:31 -0500

    Added gitbucket schema creation command

--- util/misc/gitbucket/01-setup-database.sh
+++ util/misc/gitbucket/01-setup-database.sh
@@ -10,14 +10,12 @@ PG_HOST="127.0.0.1"
 PG_PORT="5432"
 PG_USER="postgres"
 
-# Step 1: Create the database
 psql -h $PG_HOST -p $PG_PORT -U $PG_USER -c "CREATE DATABASE $DB_NAME;"
 
-# Step 2: Create the user and set the password
 psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $DB_NAME -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASSWORD';"
 
-# Step 3: Grant all privileges on the database to the user
 psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $DB_NAME -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
 
-# Step 4: Grant connect privilege on the database to the user
 psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $DB_NAME -c "GRANT CONNECT ON DATABASE $DB_NAME TO $DB_USER;"
+
+psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $DB_NAME -c "GRANT CREATE ON SCHEMA public TO $DB_USER;"
