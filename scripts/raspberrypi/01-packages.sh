280cdbb8511028b995a65765cad7a48d25f5245b
ethmth <ethan@smaze.net>
2024-01-14 17:59:31 -0500

    Updated server-install scripts

---
+++ scripts/raspberrypi/01-packages.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "This script should be run with root/sudo privileges."
+        exit 1
+fi
+
+packages="
+git
+cryptsetup
+ncdu
+postgresql-client-15
+systemd-resolved
+sshpass
+fzf
+vim
+nano
+curl
+wget
+proxychains
+docker.io
+docker-compose
+socat
+nmap
+neofetch 
+python3
+python3-dev
+python3-venv
+python3-pip
+python3-flask
+python3-flask-socketio
+net-tools
+speedtest-cli
+netcat-openbsd
+tmux
+plocate
+golang
+whois
+openvpn
+unzip
+xvfb
+jq
+ca-certificates
+caca-utils
+hexyl
+chromium
+"
+
+packages=${packages//$'\n'/ }
+packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+apt update && apt upgrade -y
+apt install $packages -y
