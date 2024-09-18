c5904a784c7f76989d6535677a720fdaff21817e
ethmth <ethan@smaze.net>
2024-09-18 18:52:29 -0400

    Added mother scripts

---
+++ scripts/mother/01-packages.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "This script should be run with root/sudo privileges."
+        exit 1
+fi
+
+packages="
+sane
+simple-scan
+fzf
+vim
+nano
+curl
+wget
+gallery-dl
+yt-dlp
+nmap
+neofetch 
+net-tools
+speedtest-cli
+netcat-openbsd
+tmux
+whois
+unzip
+jq
+ca-certificates
+feh
+hexyl
+ffmpeg
+gparted
+"
+
+packages+="
+make
+gcc
+"
+
+packages+="
+libavcodec-extra
+gstreamer1.0-libav
+gstreamer1.0-gtk3
+heif-thumbnailer
+webp
+webp-pixbuf-loader
+ffmpegthumbnailer
+tumbler
+tumbler-plugins-extra
+"
+
+packages=${packages//$'\n'/ }
+packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+apt update && apt upgrade -y
+apt install $packages -y
