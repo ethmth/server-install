fea6ea2199c57594a47ca0e777feaf34584ff35a
ethmth <ethan@smaze.net>
2024-09-24 14:37:35 -0400

    Added build image script

---
+++ programs/docker-osx/02-build-docker-image/build_image.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="sickcodes/docker-osx:naked"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
+    read -p "$IMAGE_NAME exists locally. Do you want to replace it (y/N)? " userInput
+
+    if ([ "$userInput" == "Y" ] || [ "$userInput" == "y" ]); then
+        docker rmi "$IMAGE_NAME"
+    else
+        echo "Exiting. No damage done."
+        exit 0
+    fi
+fi
+
+cd "$SCRIPT_DIR"
+
+if [ -d "Docker-OSX" ]; then
+    rm -rf "Docker-OSX"
+fi
+
+git clone https://github.com/sickcodes/Docker-OSX.git
+cd Docker-OSX
+rm -rf .git
+docker build -t "$IMAGE_NAME" --build-arg SHORTNAME=naked .
