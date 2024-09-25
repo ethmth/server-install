cf0b4e2023ed8c9ca481d09224bdf459e6299f50
ethmth <ethan@smaze.net>
2024-09-24 20:30:17 -0400

    Fixed pihole install script

--- programs/pihole/install.sh
+++ programs/pihole/install.sh
@@ -16,16 +16,17 @@ if ! [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $CONTAINER_NAME installation location")
+# LOC=$(lsblk --noheadings -o MOUNTPOINTS | grep -v '^$' | grep -v "/boot" | fzf --prompt="Select your desired $CONTAINER_NAME installation location")
 
-if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
-    echo "Nothing was selected. Run this script again with target drive mounted."
-    exit 1
-fi
+# if ([ "$LOC" == "" ] || [ "$LOC" == "Cancel" ]); then
+#     echo "Nothing was selected. Run this script again with target drive mounted."
+#     exit 1
+# fi
 
-if [ "$LOC" == "/" ]; then
-    LOC="/opt"
-fi
+# if [ "$LOC" == "/" ]; then
+#     LOC="/opt"
+# fi
+LOC="/opt"
 
 if ! [ -d "$LOC" ]; then
     echo "Your location is not available. Is the disk mounted? Do you have access?"
@@ -33,14 +34,18 @@ if ! [ -d "$LOC" ]; then
 fi
 
 LOC="$LOC/programs"
-mkdir -p $LOC/$CONTAINER_NAME
+sudo mkdir -p $LOC/$CONTAINER_NAME
+sudo chmod 777 "$LOC"
+sudo chmod 777 "$LOC/$CONTAINER_NAME"
 
 
 for file in $FILES; do
     if [ -d "$file" ]; then
         cp -r $file $LOC/$CONTAINER_NAME/
+        sudo chmod 777 $LOC/$CONTAINER_NAME/$file
     elif [ -f "$file" ]; then
         cp $file $LOC/$CONTAINER_NAME/$file
+        sudo chmod 666 $LOC/$CONTAINER_NAME/$file
     fi
 done
 
