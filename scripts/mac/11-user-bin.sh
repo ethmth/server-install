65fdfecef35473a5dca433a1ef7344fa7cd8580b
ethmth <ethan@smaze.net>
2024-11-19 17:21:01 -0500

    Removed unnecessary scripts

--- scripts/mac/11-user-bin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ${HOME}/bin
-
-echo 'export PATH="${HOME}/bin:${PATH}"' >> ${HOME}/.zshrc
-
-# echo "PATH=/usr/local/git/bin:\$PATH" >> ~/.bash_profile
-# source ~/.bash_profile
--- scripts/mac/15-disable-library-validation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "The password is alpine"
-
-sudo defaults write /Library/Preferences/com.apple.security.libraryvalidation.plist DisableLibraryValidation -bool true
-
-echo "Reboot, then log into iMessage"
