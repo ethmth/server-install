5a1e45cca6eaa07ad0fbef3a529e20fb819befd1
ethmth <ethan@smaze.net>
2024-11-19 00:00:55 -0500

    Added homebrew install to bash

--- scripts/mac/05-enable-vnc.sh
+++ scripts/mac/05-enable-vnc.sh
@@ -13,6 +13,8 @@ sudo defaults write /Library/Preferences/com.apple.VNCSettings VNCPassword -data
 # sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
 #     -status
 
+# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -off -restart -agent -privs -all -allowAccessFor -allUsers -clientopts -setvncpw -vncpw secret -setvnclegacy -vnclegacy yes
+
 echo "Go to System Preferences -> General -> Sharing -> Remote Management"
 echo "Click Computer Settings, VNC viewers may control screen with password."
 
--- scripts/mac/06-install-homebrew.sh
+++ scripts/mac/06-install-homebrew.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /Users/ethan/.bash_profile
+echo 'eval "$(/usr/local/bin/brew shellenv)"' >> /Users/ethan/.bash_profile
+eval "$(/usr/local/bin/brew shellenv)"
