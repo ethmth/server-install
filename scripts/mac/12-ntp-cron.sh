6eebf0d8b1d5230fd7ebd0d8b6d0b0f0b2308974
ethmth <ethan@smaze.net>
2025-01-17 14:42:29 -0500

    Updated crontab on mac to log

--- scripts/mac/12-ntp-cron.sh
+++ scripts/mac/12-ntp-cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "0 * * * * /usr/bin/sntp -s in.pool.ntp.org" | sudo crontab -
+echo "0 * * * * /usr/bin/sntp -s in.pool.ntp.org >> /var/log/my_cron_logs.log 2>&1" | sudo crontab -
