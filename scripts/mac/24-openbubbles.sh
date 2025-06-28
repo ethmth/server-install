52e45b0ceca099b94d2aaaed284bdc02c5ab4d4c
ethmth <ethan@smaze.net>
2025-06-28 16:46:59 -0400

    Updated hardware info script

--- scripts/mac/24-openbubbles.sh
+++ scripts/mac/24-openbubbles.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl -L0 -o ~/Desktop/Mac.Hardware.Info.zip https://github.com/OpenBubbles/Mac-Hardware-Info/releases/download/v1.2.0/Mac.Hardware.Info.zip
+# curl -o ~/Downloads/Mac.Hardware.Info.zip https://github.com/OpenBubbles/Mac-Hardware-Info/releases/latest/download/Mac.Hardware.Info.zip
+# wget https://github.com/OpenBubbles/Mac-Hardware-Info/releases/latest/download/Mac.Hardware.Info.zip
+# 
+# cd ~/Downloads
+# ditto -x -k Mac.Hardware.Info.zip .
+
+# mv "Mac Hardware Info.app" /Applications/
+
+echo "Download from this URL using Safari (for some reason wget/curl then unzip/ditto won't work):"
+echo "https://github.com/OpenBubbles/Mac-Hardware-Info/releases/latest/download/Mac.Hardware.Info.zip"
