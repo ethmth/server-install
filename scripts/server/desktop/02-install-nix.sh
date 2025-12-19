a2af11c09479ad32ff964ea9906a6ee584d9269f
ethmth <ethan@smaze.net>
2025-12-19 14:36:36 -0500

    Added install nix script

---
+++ scripts/server/desktop/02-install-nix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should NOT be run with root/sudo privileges."
+        exit 1
+fi
+
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
