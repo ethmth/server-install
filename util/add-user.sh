bda2bc4b0259bb84f55d712cae8fa46f9c45ef10
ethmth <ethan@smaze.net>
2025-12-19 14:20:58 -0500

    Modified add-user script to optionally add to groups

--- util/add-user.sh
+++ util/add-user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER=$1
+DEFAULT_GROUPS="cdrom floppy sudo audio dip video plugdev users netdev docker"
 
 if [ "$USER" == "" ]; then
     echo "Usage: ./add-user.sh <username>"
@@ -12,9 +13,17 @@ if [[ $EUID -ne 0 ]]; then
     echo "This script should be run with root/sudo privileges."
     exit 1
 fi
+IFS=' ' read -r -a GROUPS_ARRAY <<< "$DEFAULT_GROUPS"
+GROUPS_COMMA=$(IFS=,; echo "${GROUPS_ARRAY[*]}")
+
+read -p "Do you want to add the user to groups $GROUPS_COMMA (y/N)? " userInput
 
 useradd -m $USER
 
 passwd $USER
 
 usermod -s /bin/bash $USER
+
+if ([ "$userInput" == "Y" ] || [ "$userInput" == "y" ]); then
+	usermod -aG "$GROUPS_COMMA" "$USER"
+fi
