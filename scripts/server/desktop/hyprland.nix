888e0aefa77b110b596764c6a502a0bc37a6e9d1
ethmth <ethan@smaze.net>
2025-12-19 17:10:18 -0500

    Made updates

--- scripts/server/desktop/hyprland.nix
+++ scripts/server/desktop/hyprland.nix
@@ -1,8 +1,15 @@
+{ pkgs, ... }:
+
 {
   nixpkgs.config.allowUnfree = true;
   targets.genericLinux.enable = true;
   targets.genericLinux.gpu.enable = true;
   nixpkgs.config.nvidia.acceptLicense = true;
+
+  home.packages = with pkgs; [
+    mesa-demos
+    glmark2
+  ];
 
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
