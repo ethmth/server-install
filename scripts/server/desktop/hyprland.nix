5fe7368d9d51d774f52931e6401ae128c964dabf
ethmth <ethan@smaze.net>
2025-12-19 16:14:30 -0500

    Enabled gpu stuff

--- scripts/server/desktop/hyprland.nix
+++ scripts/server/desktop/hyprland.nix
@@ -1,4 +1,9 @@
 {
+  nixpkgs.config.allowUnfree = true;
+  targets.genericLinux.enable = true;
+  targets.genericLinux.gpu.enable = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
+
   programs.kitty.enable = true;
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
