511c7398b523f7fded489a2f4ce236ebfbc1774f
ethmth <ethan@smaze.net>
2025-12-19 16:10:07 -0500

    Added nix-nvidia script

---
+++ scripts/server/desktop/04-nix-nvidia.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! [[ $EUID -ne 0 ]]; then
+        echo "This script should NOT be run with root/sudo privileges."
+        exit 1
+fi
+
+ARCH="x86_64"
+DRIVER_VERSION=$(nvidia-smi --version | grep "DRIVER" | cut -d ':' -f2 | xargs)
+
+if ! [[ "$DRIVER_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "NVIDIA driver version not detected, doing nothing"
+  exit 1
+fi
+
+echo "Driver Version: $DRIVER_VERSION"
+
+echo "Fetching https://download.nvidia.com/XFree86/Linux-$ARCH/$DRIVER_VERSION/NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run"
+HASH_OUTPUT=$(nix --extra-experimental-features nix-command store prefetch-file --json "https://download.nvidia.com/XFree86/Linux-$ARCH/$DRIVER_VERSION/NVIDIA-Linux-$ARCH-$DRIVER_VERSION.run")
+
+HASH=$(echo "$HASH_OUTPUT" | jq .hash | xargs)
+echo "Hash: $HASH"
+
+echo "{
+  # Don't touch this file, it's generated automatically by 04-nix-nvidia.sh
+  targets.genericLinux.gpu.nvidia = {
+    enable = true;
+    version = \"$DRIVER_VERSION\";
+    sha256 = \"$HASH\";
+  };
+}" > "$HOME/.config/home-manager/nvidia.nix"
+
+echo "Add ./nvidia.nix to home.nix's imports, then run home-manager switch"
