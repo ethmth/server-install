a52cb7299952a1f5abf5eea295540624d45ee0c5
ethmth <ethan@smaze.net>
2025-12-20 11:24:09 -0500

    Added some more binds

--- scripts/server/desktop/per-user/02-nix/config/hyprland/binds.nix
+++ scripts/server/desktop/per-user/02-nix/config/hyprland/binds.nix
@@ -3,12 +3,19 @@
 {
   wayland.windowManager.hyprland.settings.bind =
     [
+      # App Binds
       "$mainMod,B,exec,flatpak run io.gitlab.librewolf-community"
       "$mainMod,S,exec,flatpak run com.brave.Browser --enable-features=UseOzonePlatform --ozone-platform=wayland"
+      "$mainMod,W,exec,cursor"
       "$mainMod,Q,exec,foot"
       "$mainMod,T,exec,thunar"
-      "$mainMod,Escape,exec,wlogout --protocol layer-shell -b 4 -T 400 -B 400"
       "$mainMod,R,exec,fuzzel"
+      "$mainMod,Escape,exec,wlogout --protocol layer-shell -b 4 -T 400 -B 400"
+
+      # Window Management
+      "$mainMod,C,killactive"
+      "$mainMod,F,fullscreen"
+      "$mainMod,V,togglefloating"
     ]
     ++ builtins.concatLists (
       builtins.genList (i:
