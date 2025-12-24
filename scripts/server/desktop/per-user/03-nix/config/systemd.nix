e05a15d1e54525e9cedbd9bfe125a2792c6072a9
ethmth <ethan@smaze.net>
2025-12-24 11:18:36 -0500

    Added systemd mounts settings

--- scripts/server/desktop/per-user/03-nix/config/systemd.nix
+++ scripts/server/desktop/per-user/03-nix/config/systemd.nix
@@ -2,6 +2,10 @@
 
 let
   nixProfileDir = "${config.home.homeDirectory}/.nix-profile/share";
+  ramDirs = {
+    ".cache/thumbnails" = "512M";
+    ".local/share/recently-used.xbel" = "1M"; # make this a directory to essentially disable it.
+  };
 in
 {
   # You have to explicitly add the nix profile directory to the systemd manager environment
@@ -9,5 +13,26 @@ in
   systemd.user.settings.Manager.ManagerEnvironment = {
     "XDG_DATA_DIRS" = "${nixProfileDir}:$XDG_DATA_DIRS";
   };
+
+
+  systemd.user.tmpfiles.rules =
+    map (dir: "d %h/${dir} 0700 - - -")
+      (builtins.attrNames ramDirs);
+
+  systemd.user.mounts =
+    builtins.listToAttrs
+      (map (dir: {
+        name = builtins.replaceStrings [ "/" "." ] [ "-" "-" ] (builtins.substring 1 (builtins.stringLength dir) dir);
+        value = {
+          Unit.Description = "Tmpfs mount for %h/${dir}";
+          Mount = {
+            What = "tmpfs";
+            Where = "%h/${dir}";
+            Type = "tmpfs";
+            Options = "mode=0700,size=${ramDirs.${dir}}";
+          };
+          Install.WantedBy = [ "default.target" ];
+        };
+      }) (builtins.attrNames ramDirs));
 }
 
--- scripts/server/desktop/per-user/03-nix/config/thunar.nix
+++ scripts/server/desktop/per-user/03-nix/config/thunar.nix
@@ -1,16 +1,28 @@
 { config, pkgs, ... }:
 
 {
-  # xdg.mimeApps.enable = true;
-  # xdg.mimeApps.assoc = {
-  #   "application/x-shellscript" = "foot.desktop";
-  #   "application/x-terminal-emulator" = "foot.desktop";
-  # };
-
   # Manage XFCE4 helpers.rc file
   xdg.configFile."xfce4/helpers.rc".text = ''
     TerminalEmulator=foot
     FileManager=thunar
   '';
+
+  # # Clear Thunar recently used files and make immutable
+  # xdg.dataFile."recently-used.xbel".text = ''
+  #   <?xml version="1.0" encoding="UTF-8"?>
+  #   <xbel version="1.0"
+  #         xmlns:bookmark="http://www.freedesktop.org/standards/desktop-bookmarks"
+  #         xmlns:mime="http://www.freedesktop.org/standards/shared-mime-info"
+  #   ></xbel>
+  # '';
+
+  # # Clear Thunar recently used files and make immutable
+  # home.file.".local/share/recently-used.xbel".text = ''
+  #   <?xml version="1.0" encoding="UTF-8"?>
+  #   <xbel version="1.0"
+  #         xmlns:bookmark="http://www.freedesktop.org/standards/desktop-bookmarks"
+  #         xmlns:mime="http://www.freedesktop.org/standards/shared-mime-info"
+  #   ></xbel>
+  # '';
 }
 
