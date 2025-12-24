9075057408ee10bca15bca1a67182c1ea99df3ee
ethmth <ethan@smaze.net>
2025-12-24 14:10:57 -0500

    Added install megasync script

---
+++ scripts/server/desktop/06-install-megasync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+if [[ $EUID -ne 0 ]]; then
+        echo "This script should be run with root/sudo privileges."
+        exit 1
+fi
+
+# Add Mega's GPG key
+curl -fsSL https://mega.nz/keys/MEGA_signing.key | gpg --dearmor | tee /etc/apt/keyrings/meganz-archive-keyring.gpg > /dev/null
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/meganz.sources <<EOF
+Types: deb
+URIs: https://mega.nz/linux/repo/Debian_13
+Suites: ./
+Architectures: arm64,amd64
+Signed-By: /etc/apt/keyrings/meganz-archive-keyring.gpg
+EOF
+
+# Add the repository to Apt sources:
+# echo "deb [signed-by=/etc/apt/keyrings/meganz-archive-keyring.gpg] https://mega.nz/linux/repo/Debian_13/ ./" | tee /etc/apt/sources.list.d/meganz.list
+
+# Update and install
+apt update
+apt install megasync thunar-megasync megacmd
