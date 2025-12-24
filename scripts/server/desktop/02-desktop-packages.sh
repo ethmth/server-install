107c7259c4d0617b89ba84cfa696d84921f9871d
ethmth <ethan@smaze.net>
2025-12-24 16:12:21 -0500

    Added desktop packages script

---
+++ scripts/server/desktop/02-desktop-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "This script should be run with root/sudo privileges."
+        exit 1
+fi
+
+packages="
+steam-devices
+"
+
+packages=${packages//$'\n'/ }
+packages=$(echo "$packages" | tr -s ' ' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+apt update && apt upgrade -y
+apt install $packages -y
