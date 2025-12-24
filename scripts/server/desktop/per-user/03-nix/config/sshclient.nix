9acd497fbf4b47bafd550c3f23f860aead9bc111
ethmth <ethan@smaze.net>
2025-12-24 17:13:27 -0500

    Added e-flagler to ssh client config

--- scripts/server/desktop/per-user/03-nix/config/sshclient.nix
+++ scripts/server/desktop/per-user/03-nix/config/sshclient.nix
@@ -10,6 +10,15 @@
         user = "e";
         hostname = "droplet.ethanmt.com";
       };
+      "e-lenovo" = {
+        user = "e";
+        hostname = "lenovo.ethanmt.com";
+      };
+      "e-flagler" = {
+        user = "e";
+        hostname = "192.168.0.85";
+        proxyJump = "e-lenovo";
+      };
     };
   };
 }
