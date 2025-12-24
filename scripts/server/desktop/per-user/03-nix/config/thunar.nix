3306c558deb5966fd7eed00ed7eaa5d9a025d70c
ethmth <ethan@smaze.net>
2025-12-24 14:53:04 -0500

    Moved the recently-used.xbel creation to a systemd service

--- scripts/server/desktop/per-user/03-nix/config/thunar.nix
+++ scripts/server/desktop/per-user/03-nix/config/thunar.nix
@@ -7,7 +7,20 @@
     FileManager=thunar
   '';
 
-  home.file.".local/share/recently-used.xbel/.keep" = {
-    text = "";
+  # home.file.".local/share/recently-used.xbel/.keep" = {
+  #   enable = true;
+  #   force = true;
+  #   text = "";
+  # };
+
+  # Create systemd service to ensure the directory exists before Home Manager tries to manage it
+  systemd.user.services."create-recently-used-xbel" = {
+    Service.Description = "Create recently-used.xbel directory";
+    Service.Type = "oneshot";
+    Service.ExecStart = ''
+      ${pkgs.bash}/bin/bash -c 'rm -rf "${config.home.homeDirectory}/.local/share/recently-used.xbel" && mkdir -p "${config.home.homeDirectory}/.local/share/recently-used.xbel" && chmod 755 "${config.home.homeDirectory}/.local/share/recently-used.xbel"'
+    '';
+    Service.RemainAfterExit = true;
+    Install.WantedBy = [ "default.target" ];
   };
 }
