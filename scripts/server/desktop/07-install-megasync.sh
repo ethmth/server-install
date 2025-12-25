41dfe341a4fcfc9bb0542697419c66094b0fb596
ethmth <ethan@smaze.net>
2025-12-25 00:51:31 -0500

    Fixed megasync sources

--- scripts/server/desktop/07-install-megasync.sh
+++ scripts/server/desktop/07-install-megasync.sh
@@ -9,18 +9,17 @@ fi
 # Add Mega's GPG key
 curl -fsSL https://mega.nz/keys/MEGA_signing.key | gpg --dearmor | tee /etc/apt/keyrings/meganz-archive-keyring.gpg > /dev/null
 
-# Add the repository to Apt sources:
-tee /etc/apt/sources.list.d/meganz.sources <<EOF
-Types: deb
-URIs: https://mega.nz/linux/repo/Debian_13
-Suites: ./
-Architectures: arm64,amd64
-Signed-By: /etc/apt/keyrings/meganz-archive-keyring.gpg
-EOF
+# # Add the repository to Apt sources:
+# tee /etc/apt/sources.list.d/meganz.sources <<EOF
+# Types: deb
+# URIs: https://mega.nz/linux/repo/Debian_13
+# Suites: ./
+# Architectures: arm64,amd64
+# Signed-By: /etc/apt/keyrings/meganz-archive-keyring.gpg
+# EOF
 
 # Add the repository to Apt sources:
-# echo "deb [signed-by=/etc/apt/keyrings/meganz-archive-keyring.gpg] https://mega.nz/linux/repo/Debian_13/ ./" | tee /etc/apt/sources.list.d/meganz.list
-
+echo "deb [signed-by=/usr/share/keyrings/meganz-archive-keyring.gpg] https://mega.nz/linux/repo/Debian_13/ ./" | tee /etc/apt/sources.list.d/megasync.list
 # Update and install
 apt update
 apt install megasync thunar-megasync megacmd
