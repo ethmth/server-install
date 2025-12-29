b5e84a3fbf8e6402c285b7b480359890df84ae7f
ethmth <ethan@smaze.net>
2025-12-28 21:44:28 -0500

    Added stremio docker compose config

--- programs/stremio/install.sh
+++ programs/stremio/install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="gluetun"
+CONTAINER_NAME="stremio"
 
 VOLUMES="
+aio-data
 "
 
 FILES="
-wg0.conf
 compose.yml
 "
 
-if [[ $EUID -ne 0 ]]; then
-    echo "This script should be run with root/sudo privileges."
+if ! [[ $EUID -ne 0 ]]; then
+    echo "This script should not be run with root/sudo privileges."
     exit 1
 fi
 
-LOC="/opt/docker"
+LOC="$HOME/docker"
 mkdir -p "$LOC/$CONTAINER_NAME"
 
 for file in $FILES; do
@@ -30,8 +30,6 @@ for vol in $VOLUMES; do
     mkdir -p $LOC/$CONTAINER_NAME/$vol
     chmod -R 777 $LOC/$CONTAINER_NAME/$vol
 done
-
-echo "Update/copy wg0.conf in/to installed directory. Replace domain name with IP address"
 
 echo "Installed $CONTAINER_NAME to $LOC"
 echo "Run 'docker compose up --build -d' to run"
