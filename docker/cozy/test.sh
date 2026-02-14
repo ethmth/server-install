02c7982a1472a58ce7c7b673a134af7ab4690e51
Ethan Thomas <ethan@smaze.net>
2026-02-14 16:27:05 -0500

    Maybe got something cooking

--- docker/cozy/test.sh
+++ docker/cozy/test.sh
@@ -5,3 +5,12 @@ docker compose exec stack cozy-stack instances add \
     --tz "America/New_York" \
     --passphrase password \
     domain.example
+
+
+docker exec -it cozy-stack cozy-stack instances add \
+    --apps home,banks,contacts,drive,notes,passwords,photos,settings,store \
+    --email "test@localhost:8080" \
+    --locale en \
+    --tz "America/New_York" \
+    --passphrase password \
+    localhost:8080
