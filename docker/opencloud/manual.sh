0250bcc394c4d8aa7a70e1469cefd4af9512e44b
Ethan Thomas <ethan@smaze.net>
2026-02-16 13:31:27 -0500

    Last command works

---
+++ docker/opencloud/manual.sh
@@ -0,0 +1,77 @@
+mkdir -p $HOME/opencloud/opencloud-config
+mkdir -p $HOME/opencloud/opencloud-data
+
+docker pull opencloudeu/opencloud-rolling:latest
+
+# Initialize opencloud (first time setup)
+docker run --rm -it \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -e IDM_ADMIN_PASSWORD=admin \
+    opencloudeu/opencloud-rolling:latest init
+
+
+# start opencloud
+docker run \
+    --name opencloud \
+    --rm \
+    -p 0.0.0.0:9200:9200 \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -e OC_INSECURE=true \
+    -e PROXY_HTTP_ADDR=0.0.0.0:9200 \
+    -e OC_URL=https://localhost:9200 \
+    opencloudeu/opencloud-rolling:latest
+
+docker run \
+    --name opencloud \
+    --rm \
+    -p 0.0.0.0:9200:9200 \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -e OC_INSECURE=true \
+    -e PROXY_HTTP_ADDR=0.0.0.0:9200 \
+    -e OC_URL=https://192.168.50.45:9200 \
+    opencloudeu/opencloud-rolling:latest
+
+
+# try
+docker run \
+    --name opencloud \
+    --rm \
+    -p 0.0.0.0:9200:9200 \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -v $HOME/Pictures:/var/lib/opencloud/storage/users/projects/b9af3c0a-e04c-45c7-8f70-0e43eee96ee4:ro \
+    -e OC_INSECURE=true \
+    -e PROXY_HTTP_ADDR=0.0.0.0:9200 \
+    -e OC_URL=https://192.168.50.45:9200 \
+    --user root \
+    opencloudeu/opencloud-rolling:latest
+
+
+docker run \
+    --name opencloud \
+    --rm \
+    -p 0.0.0.0:9200:9200 \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -v $HOME/Pictures:/var/lib/opencloud/storage/users/projects/b9af3c0a-e04c-45c7-8f70-0e43eee96ee4:ro \
+    -e OC_INSECURE=true \
+    -e PROXY_HTTP_ADDR=0.0.0.0:9200 \
+    -e OC_URL=https://192.168.50.45:9200 \
+    opencloudeu/opencloud-rolling:latest
+
+# try
+docker run \
+    --name opencloud \
+    --rm \
+    -p 0.0.0.0:9200:9200 \
+    -v $HOME/opencloud/opencloud-config:/etc/opencloud \
+    -v $HOME/opencloud/opencloud-data:/var/lib/opencloud \
+    -v $HOME/Pictures:/var/lib/opencloud/storage/users/projects/b9af3c0a-e04c-45c7-8f70-0e43eee96ee4/Pictures \
+    -e OC_INSECURE=true \
+    -e PROXY_HTTP_ADDR=0.0.0.0:9200 \
+    -e OC_URL=https://192.168.50.45:9200 \
+    --user root \
+    opencloudeu/opencloud-rolling:latest
